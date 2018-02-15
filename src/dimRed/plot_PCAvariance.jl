0aab0f7801ae6488e07aba5066e43a58bcc7fadf
burfel <felicia.burtscher@fu-berlin.de>
2018-02-15 19:16:45 +0000

    added histogram

--- src/dimRed/plot_PCAvariance.jl
+++ src/dimRed/plot_PCAvariance.jl
@@ -22,3 +22,40 @@ plot(pca[:explained_variance_], linewidth=2)
 axis("tight")
 xlabel("n components")
 ylabel("explained variance")
+
+
+# SVD without ScikitLearn:
+F = svdfact(data_array)
+#F[:U]
+#F[:S]
+#F[:V]
+singular_values = F[:S]
+normCon = sum(singular_values)
+normalise(x) = x/normCon
+normalisedSVs = normalise.(singular_values)
+print(normalisedSVs)
+principal_components = F[:U] * diagm(F[:S])
+#=
+#figure(1, figsize=(4, 3))
+clf()
+#axes([.2, .2, .7, .7])
+plot(singular_values, linewidth=2)
+axis("tight")
+xlabel("n components")
+ylabel("explained variance")
+=#
+
+pyplot() # Switch to using the PyPlot.jl backend
+# Todo: PLOT DOES NOT MAKE SENSE, VALUES SHOULD BE MONOTONIC DECREASING
+histogram(singular_values, linewidth=2, xlabel = "principal components", ylabel = "Singular_values", title="Singular values")
+histogram(normalisedSVs, linewidth=2, xlabel = "principal components", ylabel = "Explained variance", title="Singular values")
+#=
+# alternatively using Plots
+using Plots
+plotly() # Choose the Plotly.jl backend for web interactivity
+plot(singular_values, linewidth=2, title="Singular values")
+=#
+pyplot() # Switch to using the PyPlot.jl backend
+scatter(principal_components, linewidth=2, xlabel = "cells", ylabel ="gene expression of particular gene combination", title="Principal components")
+# .. shows that the first PC (blue) is very distinctive to the rest of the PCs
+scatter(principal_components[1,:], principal_components[2,:], linewidth=2, xlabel = "PC1", ylabel = "PC2", title="Projection onto 2 PCs")
