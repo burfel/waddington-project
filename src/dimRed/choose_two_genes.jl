2b2255e29d4e087a0f3dd5804cb8671d5c5378ea
mgh17 <madeleinehall95@gmail.com>
2018-03-01 15:17:20 +0000

    updated to call PFM and KDE functions

--- src/dimRed/choose_two_genes.jl
+++ src/dimRed/choose_two_genes.jl
@@ -1,32 +1,31 @@
 # -*- coding: utf-8 -*-
 #=
 
-Function that plots the landscape of a given data set.
+Function that plots the landscape of two chosen genes from a given data set.
 
 Note:  - run Single_cell_data/reading_in_example.jl first
+       - run simulations/PFM_reduction_2D.jl first
+       - run simulations/KDE_reduction_2D.jl first
        - 3mostcorrelated.jl gives pairs of correlated genes which are plotted
 
 Args:
     dataframe: data in the form of a dataframe or an array; here: 547 cells, 96 genes
 
 Returns:
-    -
+    -p1 a scatter of the data
+    -p2 a plot of the landscape given by the Probability Flux method
+    -p3 a plot of the landscape given by the Kernel Density Estimation method
 =#
 
 
 using Plots, DataStructures, DataFrames
 
 function two_genes(dataframe)
-
-           plotly()
-
            data_array = convert(Array,dataframe)
-
            data_array = collect(skipmissing(data_array))
            data_array = reshape(data_array,(nrow(dataframe),ncol(dataframe)))
-           data_array = data_array'
 
-           # TAKES INPUT (2 genes that should be plottet)
+           # TAKES INPUT (2 genes that should be plotted)
            (no_genes,no_cells) = size(data_array)
            gene1 = input("Gene 1: ")
            gene2 = input("Gene 2: ")
@@ -37,145 +36,109 @@ function two_genes(dataframe)
            gene1index = find(names(data).==gene1)[1]
            gene2index = find(names(data).==gene2)[1]
 
-
-           h=20
-           Ps=zeros(eye(h))
-           xspan=linspace(0.0001,20.0,h) #
-
-           total = 0
-
-           for i = 1:length(data_array[1,:])
-
-                      sol = [data_array[gene1index,i],data_array[gene2index,i]]
-                      x1=1
-                      x2=1
-                      found1=false
-                      found2=false
-                      impossible=false
-
-                      while(!(found1 && found2))
-                                 if (sol[1]<xspan[1] || sol[1]>=xspan[h] || sol[2]<xspan[1] || sol[2]>=xspan[h])
-                                            impossible=true
-                                            break;
-                                 end
-
-                                 if !(xspan[x1]<=sol[1] && sol[1]<xspan[x1+1]) && !found1
-                                            x1=x1+1
-                                 else
-                                            found1=true
-                                 end
-
-                                 if !(xspan[x2]<=sol[2] && sol[2]<xspan[x2+1]) && !found2
-                                            x2=x2+1
-                                 else
-                                            found2=true
-                                 end
-                      end
-
-                      if !impossible
-                                 Ps[x1,x2]=Ps[x1,x2]+1
-                                 total=total+1
-
-                      end
-           end
-
-           Ps=Ps/(total*(4/h)^2)+0.00001
-
-           U=-log.(Ps)
-
            p1 = scatter(data[:,gene1index],data[:,gene2index],xlabel=names(data)[gene1index],ylabel=names(data)[gene2index])
 
-           p2 = surface(xspan,xspan,U,xlabel=names(data)[gene1index],ylabel=names(data)[gene2index])
+           ###
 
+           end_state = [data_array[:,gene1index] data_array[:,gene2index]]
+           boundaries = [0.0001 0.0001; 20.0 20.0]
+           nb_bin = 20
+           dim1 = 1
+           dim2 = 2
 
-           ####################################### Kernel Density Estimation method ############################################
+           xspan = Span_2D(boundaries,nb_bin,dim1,dim2)
 
+           U = PFM_reduction_2D(end_state,boundaries,nb_bin,dim1,dim2)
+           p2 = surface(xspan[1,:],xspan[2,:],U,xlabel=names(data)[gene1index],ylabel=names(data)[gene2index])
 
-           h=20
-           Mtfinal=zeros(no_cells,2)
-           xspan=linspace(0.5,20.0,h)
-
-           total=0
-
-           for i = 1:no_cells
-               sol = [data_array[gene1index,i],data_array[gene2index,i]]
-               Mtfinal[i,1]=sol[1]
-               Mtfinal[i,2]=sol[2]
-
-           end
-
-           hps=1.06*1000^(-1/5)
-
-           function fPs(x1,x2)
-
-               sum = 0
-               for i = 1:no_cells
-                   sum=sum+exp(-((x1-Mtfinal[i,1])^2+(x2-Mtfinal[i,2])^2)/(2*(1.06*1000^(-1/5))^2))
-               end
-
-               sum=-log(sum/(1000*hps*sqrt(2*pi)))
-
-               return sum
-           end
-
-
-           Results=zeros(h,h)
-
-           for i = 1:h
-               for j = 1:h
-                   Results[i,j]=fPs(xspan[i],xspan[j])
-               end
-           end
-
-
-           ########################################################################################################################
-
-
-           p3 = surface(xspan,xspan,Results,xlabel=names(data)[gene1index],ylabel=names(data)[gene2index])
+           U_kde = KDE_reduction_2D(end_state, boundaries, nb_bin, dim1,dim2)
+           p3 = surface(xspan[1,:],xspan[2,:],U_kde,xlabel=names(data)[gene1index],ylabel=names(data)[gene2index])
 
            return(p1,p2,p3)
 end
 
+function two_genes_terminal(dataframe)
+           data_array = convert(Array,dataframe)
+           data_array = collect(skipmissing(data_array))
+           data_array = reshape(data_array,(nrow(dataframe),ncol(dataframe)))
 
-# CALL THE FUNCTION
+           # TAKES INPUT (2 genes that should be plotted)
+           (no_genes,no_cells) = size(data_array)
+           print("Gene 1: ")
+           gene1 = readline(STDIN)
+           print("Gene 2: ")
+           gene2 = readline(STDIN)
 
-# ...on the whole data set
-(p1,p2,p3) = two_genes(data) # Fgf4 and Gdf3
-plot(p1)
-plot(p2)
-plot(p3)
+           gene1 = convert(Symbol,gene1)
+           gene2 = convert(Symbol,gene2)
 
-# ...on t24 only
-(p1,p2,p3) = two_genes(t24_only) # Pou5f1 and Trp53
-plot(p1)
-plot(p2)
-plot(p3)
+           gene1index = find(names(data).==gene1)[1]
+           gene2index = find(names(data).==gene2)[1]
 
-# ...on embryonic stem cells only
-(p1,p2,p3) = two_genes(ESC_only) # Dnmt3b and Prmt7
-plot(p1)
-plot(p2)
-plot(p3)
+           p1 = scatter(data[:,gene1index],data[:,gene2index],xlabel=names(data)[gene1index],ylabel=names(data)[gene2index])
 
-#most correlated - good
-(Fgf4_Gdf3_p1,Fgf4_Gdf3_p2,Fgf4_Gdf3_p3) = two_genes(data)
-#most entropy - rubbish
-(Actb_Gapdh_p1,Actb_Gapdh_p2,Actb_Gapdh_p3) = two_genes(data)
-#most mutual information - good
-(Fgf4_Pou5f1_p1,Fgf4_Pou5f1_p2,Fgf4_Pou5f1_p3) = two_genes(data)
+           ###
 
-#Nanog and Gata6 were plotted in RDB presentation
-(Nanog_Gata6_p1,Nanog_Gata6_p2,Nanog_Gata6_p3) = two_genes(data)
-plot(Nanog_Gata6_p3)
+           end_state = [data_array[:,gene1index] data_array[:,gene2index]]
+           boundaries = [0.0001 0.0001; 20.0 20.0]
+           nb_bin = 20
+           dim1 = 1
+           dim2 = 2
 
-(x,y,z1) = two_genes(t24_only)
-plot(z1, title = "24 hours")
+           xspan = Span_2D(boundaries,nb_bin,dim1,dim2)
 
-(x,y,z2) = two_genes(t48_only)
-plot(z2, title = "48 hours")
+           U = PFM_reduction_2D(end_state,boundaries,nb_bin,dim1,dim2)
+           p2 = surface(xspan[1,:],xspan[2,:],U,xlabel=names(data)[gene1index],ylabel=names(data)[gene2index])
 
-(x,y,z3) = two_genes(t72_only)
-plot(z3, title = "72 hours")
+           U_kde = KDE_reduction_2D(end_state, boundaries, nb_bin, dim1,dim2)
+           p3 = surface(xspan[1,:],xspan[2,:],U_kde,xlabel=names(data)[gene1index],ylabel=names(data)[gene2index])
 
-(x,y,z4) = two_genes(t168_only)
-plot(z4,title = "168 hours")
+           return(p1,p2,p3)
+end
+
+## CHOOSE BACKEND
+
+## CALL THE FUNCTION
+
+## ...on the whole data set
+# (p1,p2,p3) = two_genes(data) # Fgf4 and Gdf3
+# plot(p1)
+# plot(p2)
+# plot(p3)
+#
+## ...on t24 only
+# (p1,p2,p3) = two_genes(t24_only) # Pou5f1 and Trp53
+# plot(p1)
+# plot(p2)
+# plot(p3)
+#
+## ...on embryonic stem cells only
+# (p1,p2,p3) = two_genes(ESC_only) # Dnmt3b and Prmt7
+# plot(p1)
+# plot(p2)
+# plot(p3)
+#
+# #most correlated - good
+# (Fgf4_Gdf3_p1,Fgf4_Gdf3_p2,Fgf4_Gdf3_p3) = two_genes(data)
+# #most entropy - rubbish
+# (Actb_Gapdh_p1,Actb_Gapdh_p2,Actb_Gapdh_p3) = two_genes(data)
+# #most mutual information - good
+# (Fgf4_Pou5f1_p1,Fgf4_Pou5f1_p2,Fgf4_Pou5f1_p3) = two_genes(data)
+#
+# #Nanog and Gata6 were plotted in RDB presentation
+# (Nanog_Gata6_p1,Nanog_Gata6_p2,Nanog_Gata6_p3) = two_genes(data)
+# plot(Nanog_Gata6_p3)
+#
+# (x,y,z1) = two_genes(t24_only)
+# plot(z1, title = "24 hours")
+#
+# (x,y,z2) = two_genes(t48_only)
+# plot(z2, title = "48 hours")
+#
+# (x,y,z3) = two_genes(t72_only)
+# plot(z3, title = "72 hours")
+# #
+# (x,y,z4) = two_genes(t168_only)
+#  plot(z4,title = "168 hours")
+#  #
+#(x,y,z4) = two_genes_terminal(t168_only)
