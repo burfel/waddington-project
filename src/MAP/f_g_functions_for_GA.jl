e90265720a13e8e732fa38d5295335c85e1a0ff9
lucasducrot <ducrot.lucas@gmail.com>
2018-03-07 11:32:44 +0000

    comment on the GA main, tests of GA functions

--- src/MAP/f_g_functions_for_GA.jl
+++ src/MAP/f_g_functions_for_GA.jl
@@ -1,3 +1,5 @@
+
+# 2D basic model functions
 function f_test(vector)
     results=zeros(2)
 
@@ -6,7 +8,6 @@ function f_test(vector)
 
     return results
 end
-
 function g_test(vector)
     results=zeros(2)
 
@@ -16,7 +17,7 @@ function g_test(vector)
     return results
 end
 
-
+# functions found in the Hayman paper to check the convergence of GA
 function f(vector)
     results=zeros(2)
     results[1]=vector[1]-vector[1]^3-10*vector[1]*vector[2]^2
@@ -36,14 +37,12 @@ function g(vector)
     return results
 end
 
-
-pointA=[-1.0,0.0]
+#points for the path limits and size of the path
 pointA=[0.0,2.0]
 pointB=[2.0,0.0]
 Nsize=15
 
-pop=pop_init(10,pointA,pointB,Nsize,2,f_test,g_test)
-
+#function returning the paths from a pop of trajectories
 function return_paths_from_pop(pop,id)
     results=zeros(size(pop[id].space)[1],size(pop[id].space)[2])
 
@@ -55,19 +54,23 @@ function return_paths_from_pop(pop,id)
     return results
 end
 
+#not useful currently
 for i=1:size(pop)[1]
     paths=return_paths_from_pop(pop,i)
     plot(paths[:,1],paths[:,3])
 end
 
+#run the GA, return the paths, plot the best path
 pop=GA(20,200,100,pointA,pointB,Nsize,2,f_test,g_test)
 paths=return_paths_from_pop(pop,1)
-plot(paths[:,1],paths[:,2],xlims=[0.0,3.0],ylims=[0.0,3.0],alpha=1.0)
+plot(paths[:,1],paths[:,2],xlims=[0.0,3.0],ylims=[0.0,3.0],aspect_ratio=:equal)
 
-
+#test another GA implementation
 pop=GA_mean(50,800,200,pointA,pointB,Nsize,2,f,g)
 plot(pop.space[:,1],pop.space[:,2],xlims=[-1.5,1.5],ylims=[-0.1,0.7])
 
+
+#same in 3D
 function f_3Dtest(vector)
     results=zeros(3)
 
